432cdcee1507897f2f452227f6ddf37dcbffaee8
Gianluca Tassara <gianlucatassara@proton.me>
2023-07-13 16:01:41 -0600

    Added common tickers

--- dca/constants.py
+++ dca/constants.py
@@ -1,2 +1,10 @@
 # Ticker for bitcoin in lower case (important)
 BTC_TICKER = "BTC-USD"
+
+POPULAR_TICKERS = {
+    "S&P 500": "^GSPC",
+    "Dow Jones": "^DJI",
+    "NASDAQ": "^IXIC",
+    "Gold": "GC=F",
+    "Silver": "SI=F"
+}
