dbd330883c47ac9a54f28e7c7525e35da114b5d9
Gianluca Tassara <gianlucatassara@proton.me>
2024-02-05 11:21:38 -0600

    Add CORS bug

--- bitcoinvsstocks/settings.py
+++ bitcoinvsstocks/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://bitcoinvsstocks.com/",
+    "http://bitcoinvsstocks.com",
     "http://localhost:5173",
 ]
